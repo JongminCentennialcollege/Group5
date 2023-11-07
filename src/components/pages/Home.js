04440ab8e741317455822c7dfa064bf947ab4876
Anzal-F <anzalmohamed09@gmail.com>
2023-11-07 17:50:06 -0500

    Escape

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react';
-import { gsap } from 'gsap';
-
-// Import the global CSS file (adjust the path as needed)
-import '../../App.css';
-
-function Home() {
-  useEffect(() => {
-    // Simple animation using GSAP
-    gsap.from('.intro', { opacity: 0, y: -50, duration: 1, delay: 0.5 });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="intro">Find Nearest Foodbank Locations</h1>
-        <p>Join us in the fight against hunger.</p>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>This is the home page.</p>
+        </div>
+    );
+};
 
 export default Home;
