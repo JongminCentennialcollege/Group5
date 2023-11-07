3c99ada81519d128f233e127ae14a2dbab21d544
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-11-07 16:24:11 -0500

    navigation bar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,27 +1,46 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./Navigation.css";
+import { Icon } from '@iconify/react';
+import { useState } from "react";
 
 const Navigation = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className="navbar">
+            <div className="navbar__logo">
+                <img src="../../assets/images/logo.png" alt="logo" />
+            </div>
+
+            <Icon icon="ci:hamburger-md"  className="navbar__nav-mobile" onClick={handleClick}/>
+
+            <ul className="navbar__nav">
+                <li className="nav__links">
                     <Link to="/">Home</Link>
                 </li>
-                <li>
-                    <Link to="/about">About Us</Link>
+
+                <li className="nav__links">
+                    <Link to="/about" activeClassName="active">About Us</Link>
                 </li>
-                <li>
-                    <Link to="/foodbanks">Food Banks</Link>
+
+                <li className="nav__links">
+                    <Link to="/foodbanks" activeClassName="active">Food Banks</Link>
                 </li>
-                <li>
-                    <Link to="/partners">Partners</Link>
+
+                <li className="nav__links">
+                    <Link to="/partners" activeClassName="active">Partners</Link>
                 </li>
-                <li>
-                    <Link to="/contact">Contact Us</Link>
+
+                <li className="nav__links">
+                    <Link to="/contact" activeClassName="active">Contact Us</Link>
                 </li>
-                <li>
-                    <Link to="/donate">Donate</Link>
+
+                <li className="nav__links">
+                    <Link to="/donate" activeClassName="active">Donate</Link>
                 </li>
             </ul>
         </nav>
