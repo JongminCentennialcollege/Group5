1bf34d60f194b4e3de25faa0920a1472ac1afc69
Ujjwal Poudel <upoudel1@my.centennialcollege.ca>
2023-11-07 21:14:52 -0500

    Changes in home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,28 +8,4 @@ const Home = () => {
     );
 };
 
-<<<<<<< HEAD
-// css for home page not for the whole app
-import './Home.css';
-
-function Home() {
-  useEffect(() => {
-    // Simple animation using GSAP
-    gsap.from('.intro', { opacity: 0, y: -50, duration: 1, delay: 0.5 });
-  }, []);
-
-  // Give me your intagram id :)
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <h1 className="intro">Find Nearest Foodbank Locations</h1>
-        <p>Join us in the fight against hunger.</p>
-      </header>
-    </div>
-  );
-}
-
 export default Home;
-=======
-export default Home;
->>>>>>> 111eebd1118ead48e6db050cfd646871dc40e41c
