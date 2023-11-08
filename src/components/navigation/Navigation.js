3ccc65452fd89db656307bc5a30cb03a74337abe
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-11-08 09:38:10 -0500

    Navbar fixed

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navigation.css";
 import { Icon } from '@iconify/react';
 import { useState } from "react";
-import Nav from "./Nav";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
 
@@ -12,42 +12,38 @@ const Navigation = () => {
 
     return (
         <>
-            <nav className={click ? "Navbar-none" : "Navbar-flex"} >
-                <div className={click ? "second-display-flex" : "second-display-none"}>
-                    <div className="navbar__logo">
-                        <img src="../../assets/images/logo.png" alt="logo" />
-                    </div>
-
-                    <Icon icon="ci:hamburger-md" className="navbar__nav-mobile" onClick={handleClick} />
+            <nav className={click ? "Navbar__column" : "Navbar-flex"}>
+                <div className= "navbar__logo">
+                    <h1>Find Your Food</h1>
                 </div>
-                
-                <Nav propsClass = {click ? "navbar__navbar" : "navbar__navbar-mobile" }/>
-            </nav>
 
-            {/* <div className={click ? "navbar__navbar" : "navbar__navbar-mobile"}>
-                <ul>
-                    <li className="nav__links">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="nav__links">
-                        <Link to="/about" activeClassName="active">About Us</Link>
-                    </li>
-                    <li className="nav__links">
-                        <Link to="/foodbanks" activeClassName="active">Food Banks</Link>
-                    </li>
-                    <li className="nav__links">
-                        <Link to="/partners" activeClassName="active">Partners</Link>
-                    </li>
-                    <li className="nav__links">
-                        <Link to="/contact" activeClassName="active">Contact Us</Link>
-                    </li>
-                    <li className="nav__links">
-                        <Link to="/donate" activeClassName="active">Donate</Link>
-                    </li>
-                </ul>
-            </div> */}
+                <Icon icon="ci:hamburger-md" className="navbar__nav-mobile" onClick={handleClick} />  
+            
+
+                <div className={click ? "navbar__navbar" : "navbar__navbar-mobile"}>
+                    <ul className={click ? "nav__items-flex" : "nav__items"}>
+                        <li className="nav__links">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="nav__links">
+                            <Link to="/about" activeClassName="active">About Us</Link>
+                        </li>
+                        <li className="nav__links">
+                            <Link to="/foodbanks" activeClassName="active">Food Banks</Link>
+                        </li>
+                        <li className="nav__links">
+                            <Link to="/partners" activeClassName="active">Partners</Link>
+                        </li>
+                        <li className="nav__links">
+                            <Link to="/contact" activeClassName="active">Contact Us</Link>
+                        </li>
+                        <li className="nav__links">
+                            <Link to="/donate" activeClassName="active">Donate</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
-        
     );
 };
 
