245f445b2d0dc31fe281f7a513257df65a1497f6
Anzal-F <anzalmohamed09@gmail.com>
2023-11-08 13:03:13 -0500

    ENTER

--- src/components/pages/Partner.js
+++ src/components/pages/Partner.js
@@ -1,73 +1,38 @@
 import React from 'react';
+import './Partners.css'; // Import the CSS file
 
-function Banner(props) {
-  const bannerStyle = {
-    maxWidth: '100%',
-  };
+import topLeftImage from '../../assets/images/Vector.png';
+import bottomLeftImage1 from '../../assets/images/Rectangle 22.png';
+import bottomLeftImage2 from '../../assets/images/Rectangle 23.png';
+import bottomLeftImage3 from '../../assets/images/Rectangle 24.png';
+import topRightImage from '../../assets/images/Group 13.png';
+import bottomLeftImage11 from '../../assets/images/Rectangle 22 (1).png';
+import bottomLeftImage22 from '../../assets/images/Rectangle 23 (1).png';
+import bottomLeftImage33 from '../../assets/images/Rectangle 24 (1).png';
+import topRightImage1 from '../../assets/images/image 10.png';
 
+function PartnersPage() {
   return (
-    <div className="banner">
-      <img src={require('../../assets/images/' + props.imageUrl)} alt={props.altText} style={bannerStyle} />
+    <div className="partners-page">
+      <div className="partner-div" style={{ backgroundColor: '#00715D' }}>
+        <p>Our Partners!</p>
+        <img src={topLeftImage} alt="Top Left" className="top-left-image" />
+      </div>
+      <div className="partner-div" style={{ backgroundColor: '#ff8352' }}>
+        <p>Daily Bread </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.</p>        <img src={bottomLeftImage1} alt="Bottom Left 1" className="bottom-left-image" />
+        <img src={bottomLeftImage2} alt="Bottom Left 2" className="bottom-left-image" />
+        <img src={bottomLeftImage3} alt="Bottom Left 3" className="bottom-left-image" />
+        <img src={topRightImage} alt="Top Right" className="top-right-image" />
+      </div>
+      <div className="partner-div" style={{ backgroundColor: '#eef5f4' }}>
+        <img src={bottomLeftImage11} alt="Bottom Left 3" className="bottom-left-image" />
+        <img src={bottomLeftImage22} alt="Bottom Left 2" className="bottom-left-image" />
+        <img src={bottomLeftImage33} alt="Bottom Left 1" className="bottom-left-image" />
+        <img src={topRightImage1} alt="Top Right" className="top-right-image" />
+      </div>
     </div>
   );
 }
 
-function Partner(props) {
-  const partnerStyle = {
-    textAlign: 'center',
-    padding: '20px',
-    border: '1px solid #ddd',
-    borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-    maxWidth: '300px',
-  };
-
-  const imageStyle = {
-    maxWidth: '100px',
-    height: 'auto',
-    borderRadius: '50%',
-  };
-
-  return (
-    <div className="partner" style={partnerStyle}>
-      <img src={require('../../assets/images/' + props.logoUrl)} alt={props.partnerName} style={imageStyle} />
-      <p><strong>{props.partnerName}</strong></p>
-      <p>{props.description}</p>
-    </div>
-  );
-}
-
-export default function PartnersPage() {
-  const partnersPageStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '20px',
-  };
-
-  return (
-    <div className="partners-page" style={partnersPageStyle}>
-      <Banner imageUrl="Rectangle 6.png" altText="Banner 1" />
-      <Banner imageUrl="Group 15.png" altText="Banner 2" />
-      <Banner imageUrl="Group 16.png" altText="Banner 3" />
-      
-      <Partner
-        logoUrl="Rectangle 22.png"
-        partnerName="Partner 1"
-        description="Description for Partner 1"
-      />
-
-      <Partner
-        logoUrl="Rectangle 23.png"
-        partnerName="Partner 2"
-        description="Description for Partner 2"
-      />
-
-      <Partner
-        logoUrl="Rectangle 24.png"
-        partnerName="Partner 3"
-        description="Description for Partner 3"
-      />
-    </div>
-  );
-}
+export default PartnersPage;
